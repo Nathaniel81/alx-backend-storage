deb18a587d05eef39f70d168f4d0d1d004c37af8
Nathaniel81 <nathanield356@gmail.com>
2023-04-07 21:23:56 +0300

    Email validation to sent

--- 0x00-MySQL_Advanced/playground/5-valid_email.sql
+++ 0x00-MySQL_Advanced/playground/5-valid_email.sql
@@ -1,10 +1,18 @@
 DELIMITER //
+IF EXISTS (
+    SELECT * FROM information_schema.triggers 
+    WHERE trigger_name = 'trig_users_email_update' 
+    AND trigger_schema = 'holberton'
+) THEN
+    DROP TRIGGER IF EXISTS trig_users_email_update;
+END IF;
 
 CREATE TRIGGER trig_users_email_update
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-	IF NEW.email <> OLD.email THEN
-		SET NEW.valid_email = 0;
-	END IF;
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
 END;
 //
+DELIMITER ;
