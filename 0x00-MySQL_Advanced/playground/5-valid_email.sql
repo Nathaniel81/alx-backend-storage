ff03bc4c16962cf2cde2f22476d9e0d9679c16b7
Nathaniel81 <nathanield356@gmail.com>
2023-04-07 21:14:51 +0300

    Email validation to sent

--- 0x00-MySQL_Advanced/playground/5-valid_email.sql
+++ 0x00-MySQL_Advanced/playground/5-valid_email.sql
@@ -2,7 +2,7 @@ DELIMITER //
 CREATE TRIGGER trig_users_email_update
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-	IF NEW.email <> OLD.email
+	IF NEW.email <> OLD.email THEN
 		SET NEW.valid_email = 0;
 	END IF;
 END;
