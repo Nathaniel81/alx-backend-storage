064306b3f7304345626b44c7fd664576738cc5a8
Nathaniel81 <nathanield356@gmail.com>
2023-04-08 10:26:26 +0300

    Average score

--- 0x00-MySQL_Advanced/playground/7-average_score.sql
+++ 0x00-MySQL_Advanced/playground/7-average_score.sql
@@ -1,22 +1,21 @@
-DELIMITER //
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score DECIMAL(10, 2);
-    DECLARE total_count INT;
-    -- DECLARE avg_score DECIMAL(10, 2);
-    
-    -- Calculate total score and count for the given user
-    SELECT SUM(score), COUNT(*) INTO total_score, total_count
-    FROM corrections
-    WHERE user_id = user_id;
-    
-	UPDATE users
-        SET users.average_score = IF(total_count = 0, 0, total_score / total_count)
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
+
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
         WHERE users.id = user_id;
-    
-    -- Update average score in users table
-    -- UPDATE users SET average_score = avg_score WHERE id = user_id;
-    
-END //
-DELIMITER ;
+END $$
+DELIMITER 
