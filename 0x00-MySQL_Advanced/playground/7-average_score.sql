6ead269679767708478e1dd7c0ab329e59d800a1
Nathaniel81 <nathanield356@gmail.com>
2023-04-08 09:35:33 +0300

    Average score

---
+++ 0x00-MySQL_Advanced/playground/7-average_score.sql
@@ -0,0 +1,14 @@
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE sum_scores INT;
+	DECLARE count_corrections INT;
+	DECLARE average_score FLOAT;
+
+	SELECT SUM(score) INTO sum_scores FROM corrections WHERE user_id = user_id;
+	SELECT COUNT(*) INTO count_corrections FROM corrections WHERE user_id = user_id;
+
+	average_score = sum_scores / count_corrections;
+	UPDATE users SET average_score = average_score WHERE id = user_id;
+END //
+DELIMITER ;
