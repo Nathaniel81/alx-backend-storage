f93051fb00b7177f1a4e9909bb6480b397b6761c
Nathaniel81 <nathanield356@gmail.com>
2023-04-08 10:12:33 +0300

    Average score

--- 0x00-MySQL_Advanced/playground/7-average_score.sql
+++ 0x00-MySQL_Advanced/playground/7-average_score.sql
@@ -2,26 +2,24 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    -- Declare variables to hold the sum of scores and count of corrections
-    DECLARE sum_scores INT DEFAULT 0;
-    DECLARE count_corrections INT DEFAULT 0;
-    DECLARE average_score DECIMAL(5, 2);
-
-    -- Calculate the sum of scores for the given user_id
-    SELECT SUM(score) INTO sum_scores FROM corrections WHERE user_id = user_id;
+    DECLARE total_score DECIMAL(10, 2);
+    DECLARE total_count INT;
+    DECLARE avg_score DECIMAL(10, 2);
     
-    -- Calculate the count of corrections for the given user_id
-    SELECT COUNT(*) INTO count_corrections FROM corrections WHERE user_id = user_id;
+    -- Calculate total score and count for the given user
+    SELECT SUM(score), COUNT(*) INTO total_score, total_count
+    FROM corrections
+    WHERE user_id = user_id;
     
-    -- Calculate the average score, handling division by zero
-    IF count_corrections > 0 THEN
-        SET average_score = sum_scores / count_corrections;
+    -- Calculate average score
+    IF total_count > 0 THEN
+        SET avg_score = total_score / total_count;
     ELSE
-        SET average_score = 0;
+        SET avg_score = 0;
     END IF;
     
-    -- Update the average_score column in the users table for the given user_id
-    UPDATE users SET average_score = average_score WHERE id = user_id;
+    -- Update average score in users table
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
+    
 END //
-
 DELIMITER ;
