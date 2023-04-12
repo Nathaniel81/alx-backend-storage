9bb445dda995f002d6cb14fc611ccab7de3a0a67
Nathaniel81 <nathanield356@gmail.com>
2023-04-12 20:44:20 +0300

    readme

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""exercise module"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache():
+    """Cache class"""
+    def __init__(self):
+        """Initialize the cache"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Takes in a data argument and returns a string"""
+        key = uuid.uuid4()
+        self.redis.set(key, data)
+        return key
