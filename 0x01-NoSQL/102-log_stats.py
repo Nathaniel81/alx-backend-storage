1c088489d6030d4a09fd862f7fce517e7b56c492
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-07-19 21:08:04 +0300

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,46 +1,30 @@
 #!/usr/bin/env python3
-""" 12-log_stats module """
+"""
+102-log_stats module
+"""
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    count = logs_collection.count_documents({})
-    print("{} logs".format(count))
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx = client.logs.nginx
+    logs = nginx.count_documents({})
+    print(f"{logs} logs")
     print("Methods:")
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        meth_count = logs_collection.count_documents(
-            {"method": {
-                "$eq": method
-            }})
-        print("\tmethod {}: {}".format(method, meth_count))
-
-    status_checks = logs_collection.count_documents(
-        {"$and": [{
-            "method": {
-                "$eq": 'GET'
-            }
-        }, {
-            "path": "/status"
-        }]})
-    print("{} status check".format(status_checks))
-
-    top_ips = logs_collection.aggregate([{
-        "$group": {
-            "_id": "$ip",
-            "count": {
-                "$sum": 1
-            }
-        }
-    }, {
-        '$sort': {
-            "count": -1
-        }
-    }, {
-        "$limit": 10
-    }])
-
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = nginx.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    stats = nginx.count_documents({"path": "/status"})
+    print(f"{stats} status check")
     print("IPs:")
-    for doc in top_ips:
-        print("\t{}: {}".format(doc.get("_id"), doc.get("count")))
+    ips = nginx.aggregate(
+        [
+            {"$group": {"_id": "$ip", "sum": {"$sum": 1}}},
+            {"$sort": {"sum": -1}},
+            {"$limit": 10},
+        ]
+    )
+    for ip in ips:
+        sum = ip["sum"]
+        ip = ip["_id"]
+        print(f"\t{ip}: {sum}")
